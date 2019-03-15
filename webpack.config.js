250dc7f911d2beb492f9977fd608cb1d899a8044
Black-Tomcat <tomcat.frantz@gmail.com>
2019-03-16 10:20:31 +1100

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    mode: "development",
+    devtool: "heap-module-inline-source-map",
+    target: 'electron-main',
+    entry: {
+        index: "./src/index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    watch: false,
+    watchOptions: {
+        aggregateTimeout: 1000,
+        poll: 1000,
+        ignored: /node_modules/
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: "/node_modules/",
+                use: {
+                    loader: "babel-loader",
+                }
+            }, {
+                test: /\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            }, {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }, {
+                test: /\.png?$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: './resources/[name].[ext]'
+                    }
+                }
+            }, {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        name: './assets/[name].[ext]'
+                    }
+                }
+            }
+        ]
+    },
+    node: {
+        fs: 'empty'
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html",
+            chunks: ["index"]
+        })
+    ]
+};
